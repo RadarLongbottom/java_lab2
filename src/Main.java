f96f5002a857757bca348da806a466e5b273caf7
RadarLongbottom <aleksander.skierka@gmail.com>
2022-01-07 15:40:56 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,16 @@ public class Main {
         /* zad.1 Napisz program który będzie iterował od 100 do 0,
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
+        System.out.println("\n");
+        for( int i = 100; i > 0; i--)
+        {
+            if (i % 6 == 0)
+            {
+                System.out.print(i + " ");
+                i--;
+            }
 
+        }
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy

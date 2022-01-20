bf8ee6cfb3bc54bf9fa7f1c0459119824433fc8a
aleks <aleksander.skierka@gmail.com>
2022-01-20 16:53:00 +0100

    v

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    public static int[] zad1(int pierwsze) {
+        var tab = new int[pierwsze + 1];
+        var x = 0;
+        for (int a = 40; a >= pierwsze; a--) {
+            tab[x] = a;
+            x++;
+        }
+        for (int a = 0; a < tab.length; a++) {
+            System.out.println(tab[a] + ",");
+        }
+        return tab;
+    }
+    public static String[] zad2(){
+        Scanner scan = new Scanner(System.in);
+        String[] tab1 = new String[6];
+
+    for (int k = 0; k < tab1.length; k++)
+    {
+        tab1[k] = scan.nextLine();
+    }
+    for (int k = tab1.length -1; k >= 0; k--){
+        System.out.println(tab1[k]);
+    }
+    return tab1;
+    }
+
+    public static void zad3(String name){
+        System.out.println("Imię: " + name );
+    }
+    public static void zad3(String name, String surname){
+        System.out.println("Imię: " + name + " Nazwisko: " + surname);
+    }
+    public static void zad3(String name, String surname, int age){
+        System.out.println("Imię: " + name + " Nazwisko: " + surname + " Wiek: " + age);
+    }
+    public static int zad4(int A, int B){
+        int wynik = A + B;
+        System.out.println(wynik);
+        return wynik;
+    }
+    public static int zad4(int A, int B, int C){
+        int wynik = A + B + C;
+        System.out.println(wynik);
+        return wynik;
+    }
+
 
     public static void main(String[] args) {
         /* ćwiczenie 5 Metody */
@@ -12,6 +59,8 @@ public class Main {
         c) napisać metodę, która wypisze otrzymane wartości,
         d) wstawić wywołanie metody do metody main
         */
+        System.out.println("Zadanie 1");
+        var tab = zad1(20);
 
         /* zad.2
         a) stworzyc tablicę typu String[6],
@@ -19,6 +68,9 @@ public class Main {
             (użyć do tego pętli i klasy Scanner),
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
+        System.out.println("Zadanie 2");
+        System.out.println("Wprowadź 6 wartości do tablicy: ");
+        zad2();
 
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
@@ -28,6 +80,10 @@ public class Main {
         c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
+        System.out.println("Zadanie 3");
+        zad3("Aleksander ");
+        zad3("Aleksander", "Skierka");
+        zad3("Aleksander", "Skierka", 21);
 
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
@@ -36,8 +92,11 @@ public class Main {
             którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
-
-
+        System.out.println("Zadanie 4");
+        int zmiennaA = zad4(5, 7);
+        int zmiennaB = zad4(5, 14, 22);
+        int wynik = zmiennaA + zmiennaB;
+        System.out.println(wynik);
     }
 
 }
